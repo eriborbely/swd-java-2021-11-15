31ea2bf8394904911f4fb95c1fc28007a6c440fe
eriborbely <eri.borbely@gmail.com>
2021-11-16 16:26:02 +0100

    grid

--- src/test/java/welcome/WelcomeTest.java
+++ src/test/java/welcome/WelcomeTest.java
@@ -4,31 +4,69 @@ import extension.SeleniumTest;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import java.net.URL;
+import org.openqa.selenium.WebElement;
 
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SeleniumTest
 class WelcomeTest {
 
-
     @Test
-    void testTypeToInputThenCheckValue(WebDriver driver, URL url){
+    void testSayHello(WebDriver driver, URL url) {
         driver.get(url + "/welcome");
 
-        var input = driver.findElement(By.id("name-input"));
-        input.sendKeys("teszt");
+        driver.findElement(By.id("name-input")).sendKeys("John Doe");
+        driver.findElement(By.id("welcome-button")).click();
 
-        var submit = driver.findElement(By.id("welcome-button"));
-        submit.click();
-
-        var text = driver.findElement(By.id("welcome-div"));
-
-        assertEquals("Hello teszt!", text.getText());
-
+        var message = driver.findElement(By.id("welcome-div"))
+                .getText();
+        assertEquals("Hello John Doe!", message);
     }
 
+    @Test
+    void testContext(WebDriver driver, URL url) {
+        driver.get(url + "/welcome");
 
+        var outerDiv = driver.findElement(By.id("outer-div"));
 
+        var p = outerDiv.findElement(By.tagName("p"));
+        // find element meghívható bármilyen WebElement kontextusában
 
+        assertEquals("This is a paragraph in a div.", p.getText());
+    }
+
+    @Test
+    void testListItems(WebDriver driver, URL url) {
+        driver.get(url + "/welcome");
+
+        var items = driver.findElements(By.tagName("li"));
+
+//        var cookies = List.of("Pogácsa", "Kakaóscsiga", "Muffin");
+//        for (int i = 0; i < items.size(); i++) {
+//            assertEquals(cookies.get(i), items.get(i).getText());
+//        }
+
+//        assertEquals(List.of("Pogácsa", "Kakaóscsiga", "Muffin"),
+//                items.stream().map(WebElement::getText).toList());
+
+//        assertEquals(List.of("Pogácsa", "Kakaócsiga", "Muffin"),
+//                items.stream().map(WebElement::getText).collect(Collectors.toList()));
+
+        assertThat(items)
+                .extracting(WebElement::getText)
+//                .containsExactly("Pogácsa", "Kakaóscsiga", "Muffin");
+                .containsExactlyInAnyOrder("Pogácsa", "Kakaócsiga", "Muffin");
+
+        var message = "Hewllo World";
+//        assertTrue(message.startsWith("Hello"));
+
+        assertThat(message)
+                .startsWith("Hello");
+
+    }
 }
