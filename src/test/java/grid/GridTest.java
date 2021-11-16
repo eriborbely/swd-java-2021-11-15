fcf3acecbad8338d271fdc62b3a72ceddf3e2e1a
eriborbely <eri.borbely@gmail.com>
2021-11-16 16:57:44 +0100

    relative test

--- src/test/java/grid/GridTest.java
+++ src/test/java/grid/GridTest.java
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
 
 
 @SeleniumTest
@@ -20,6 +21,26 @@ class GridTest {
     @Test
     void test(WebDriver driver, URL url) {
         driver.get(url + "/grid");
+
+        var row = driver.findElements(By.cssSelector("tr:nth-child(2) > td"));
+
+        assertThat(row)
+                .extracting(WebElement::getText)
+                .containsExactly("4", "5", "6");
+
+    }
+
+    @Test
+    void testRelative(WebDriver driver, URL url) {
+        driver.get(url + "/grid");
+
+        var cell5 = driver.findElement(By.cssSelector("tr:nth-child(2) > td:nth-child(2)"));
+        var cell2 = driver.findElement(with(By.cssSelector("td")).below(cell5));
+
+        assertEquals("2", cell2.getText());
+
+
+
     }
 
 }
